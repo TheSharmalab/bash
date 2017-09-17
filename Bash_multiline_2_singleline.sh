453f1649e24fb46814b80fd621f098295f6e842d
TheSharmalab <Nik@macintosh-2.local>
2017-09-17 19:06:18 +0100

    update

--- Bash_multiline_2_singleline.sh
+++ Bash_multiline_2_singleline.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ROOT="~/Data_LHA/COMP/dti"
 
-cat test.txt | xargs | sed -e 's/ /;/g' > output.txt  
+cat test.txt | xargs | sed -e 's/ /;/g' > output.txt   # uses ; as a separator
+
+cat test.txt | xargs  # uses a space as a separator
