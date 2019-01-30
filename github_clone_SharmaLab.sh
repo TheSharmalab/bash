983e2eb656efec4209f7954d2bd367ad39f32d20
NS <nikhil.sharma@ucl.ac.uk>
2019-01-30 08:47:07 +0000

    updated

---
+++ github_clone_SharmaLab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# This clones the repositories of the Sharmalab
+# It creates a directory for each repository so execute within your "script " directory
+pwd
+git clone git@github.com:TheSharmalab/MRI.git
+git clone git@github.com:TheSharmalab/bash.git
+git clone git@github.com:TheSharmalab/legion.git
+git clone git@github.com:TheSharmalab/TMS_EEG.git
+
